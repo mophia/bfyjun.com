07f11295ab758b463e1595490974cd0ba666f8a0
Bingo <faaa@live.com>
2022-04-16 09:34:35 +0800

    🥓 init

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  let name = "BFY❤️JUN"
+  const now = new Date()
+  const start = new Date("08/10/2021")
+  let Difference_In_Time = now.getTime() - start.getTime(); 
+	var Difference_In_Days = Math.floor(Difference_In_Time / (1000 * 3600 * 24));
+
+</script>
+
+<main>
+  <h1>Hello {name}</h1>
+  <h2>今天是布布和俊俊在一起的第{Difference_In_Days}天～～
+</main>
+
+<style>
+  h1 {
+   text-align: center;
+  }
+  h2 {
+   font-size: 40px;
+   text-align: center;
+  }
+</style>
